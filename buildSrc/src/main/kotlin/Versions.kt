ad3c79690369c4b87ef6da122740937b3760eaa0
ligi <ligi@ligi.de>
2020-03-25 03:18:26 +0100

    Use jupiter 5.6.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val kotlin = "1.3.71"
     const val versions_plugin = "0.28.0"
-    const val jupiter = "5.6.0"
+    const val jupiter = "5.6.1"
     const val okio: String = "2.4.0"
 }
